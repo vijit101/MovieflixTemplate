30731adb09e9f3ac90966b1911c19b007c030560
Shubham <40598861+vijit101@users.noreply.github.com>
2025-05-27 20:03:44 +0530

    added css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-
+import movieCard from "./moviecard.js";
 function App() {
   return (
-    <h1>Testing movie App</h1>
+    <>
+     <h1>Testing movie App</h1>
+     <movieCard/>
+    </>
+   
   );
 }
 
--- src/moviecard.js
+++ src/moviecard.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-class movieCard extends React.Component{
+export default class movieCard extends React.Component{
     render(){
         return(
-        <h1>This is a test </h1>
+        <h1>Movie card</h1>
         );
     }
 }
